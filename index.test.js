e4f1f4b665dbf1d2fec96802083c6a80fd63cbd7
Neha <nehaprajapati1150@gmail.com>
2021-12-22 16:42:52 +0530

    mocha

--- index.test.js
+++ index.test.js
@@ -1,15 +1,18 @@
 const assert=require('assert')   //for repeatition of codes
 const {forEach,map}=require('./index');
-const test=(desc,fn)=>{
-  console.log('---',desc);
- try{
-  fn();
- }catch(err){
-     console.log(err.message);
- }
-}
+// const test=(desc,fn)=>{
+//   console.log('---',desc);
+//  try{
+//   fn();
+//  }catch(err){
+//      console.log(err.message);
+//  }
+// }
 
-test('The forEach function',()=>{
+//using mocha now  -- on terminal write mocha in index.test.js
+//mocha is replacement of above test code and result in very nice way
+
+it('The forEach function',()=>{
     let sum=0;
     forEach([1,2,3],(value)=>{
      sum+=value;
@@ -18,7 +21,7 @@ test('The forEach function',()=>{
     assert.strictEqual(sum,6,'Expected forEach to sum the array')   //result,expected value,err message
 });
 
-test('The map function',()=>{
+it('The map function',()=>{
     const result=map([1,2,3],value=>{
         return value * 2;
     
